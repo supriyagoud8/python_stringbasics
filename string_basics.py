32638607cad920441e7bb126cbdb630ff786f5b8
supriyagoud8 <113579979+supriyagoud8@users.noreply.github.com>
2022-09-16 15:21:05 +0530

    Add files via upload

---
+++ string_basics.py
@@ -0,0 +1,22 @@
+from queue import PriorityQueue
+
+
+print('hello'*5)
+print('hello '*5)
+print('hello\n'*5)#\n is for vertical result
+print('hello\n'*5,end='')
+print('hello\n'*5,end='')
+
+#concatenation
+#string can only combined with a string
+print('hi'+'bye')
+print('hi','bye')#one coma is a single space
+print('hi'+' '+'bye')
+print('i'+' '+'am'+' '+'supriya')
+
+print('hi'+'5')
+print('hi '+'5') 
+print('i','am','supriya')
+print('i','am','supriya',sep='  ')
+print('i','am','supriya',sep='       ')
+print('1','2','3','4','5',sep='-')
